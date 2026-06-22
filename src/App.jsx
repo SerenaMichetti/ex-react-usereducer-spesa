f5f06c7233f4b4a7b4dc0d29ed0f1703eb6530f6
Serena <sere.michetti@icloud.com>
2026-06-22 17:24:06 +0200

    milestone 1 completata

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 import { useState } from 'react'
-import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
+  const products = [
+    { name: 'Mela', price: 0.5 },
+    { name: 'Pane', price: 1.2 },
+    { name: 'Latte', price: 1.0 },
+    { name: 'Pasta', price: 0.7 },
+  ];
   // const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <h1>Lista prodotti:</h1>
+      <ul>
+        {products.map((products, i) => (
+          <li key={i}>
+            {products.name} {products.price.toFixed(2)}€
+          </li>))}
+      </ul>
+
     </>
   )
 }
 
-export default App
+export default App;
