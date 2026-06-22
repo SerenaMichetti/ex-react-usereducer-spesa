03b8c06ac903eb53fc2da070ea507c95e56cab26
Serena <sere.michetti@icloud.com>
2026-06-22 19:22:08 +0200

    milestone 2 completata

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,48 @@ function App() {
     { name: 'Latte', price: 1.0 },
     { name: 'Pasta', price: 0.7 },
   ];
-  // const [count, setCount] = useState(0)
+  const [addedProducts, setaddedProducts] = useState([])
+  console.log(addedProducts);
+
+  // prende in input un prodotto 
+  // - se il prodotto è già presente nel carrello => non fa nulla
+  // - se il prodotto è assente dal carrello => lo aggiunge al carrello
+  function addToCart(product) {
+    const isAdded = addedProducts.some(p => p.name === product.name)
+    if (isAdded) {
+      return
+    }
+    // lo aggiungo 
+    setaddedProducts(curr =>
+      [
+        ...curr,
+        {
+          ...product,
+          quantity: 1
+        }
+      ]
+    )
+  }
 
   return (
     <>
       <h1>Lista prodotti:</h1>
       <ul>
-        {products.map((products, i) => (
+        {products.map((p, i) => (
           <li key={i}>
-            {products.name} {products.price.toFixed(2)}€
+            {p.name} {p.price.toFixed(2)}€
+            <button onClick={() => addToCart(p)}>Aggiungi al carrello</button>
           </li>))}
       </ul>
-
+     {addedProducts.length > 0 && (<>
+      <h2>Prodotti nel carrello:</h2>
+           <ul>
+        {addedProducts.map((p, i) => (
+          <li key={i}>
+           {p.name} {p.price.toFixed(2)}€ x {p.quantity}
+          </li>))}
+      </ul>
+     </>)}
     </>
   )
 }
